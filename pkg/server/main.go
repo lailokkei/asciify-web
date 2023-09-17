32cbf1cf0e0d1fef06490cdbffb9002f2cd378dc
toodemhard <lailokkay@gmail.com>
2023-09-18 00:37:28 +0100

    Add page re-rendering flag
    --dev flag
    Templates are executed on every page request to enable viewing html
    changes immediately.

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -2,26 +2,36 @@ package server
 
 import (
 	"asciify-web/pkg/render"
+	"flag"
 	"fmt"
 	"net/http"
 )
-
-// func catch404(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
-//     fn := http
-//     return
-// }
 
 const staticDir = "static"
 const pagesDir = "pages"
 const port = "8080"
 
+func PageRerender(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		render.RenderTemplates()
+		h.ServeHTTP(w, r)
+	})
+}
+
 func Start() {
-	render.RenderTemplates()
+	var devFlag bool
+	flag.BoolVar(&devFlag, "dev", false, "Enables page hot reloading")
+	flag.Parse()
 
 	fs := http.FileServer(http.Dir(staticDir))
-	ps := http.FileServer(http.Dir(pagesDir))
+	pageServer := NoCache(http.FileServer(http.Dir(pagesDir)))
 
-	http.Handle("/", NoCache(ps))
+	if devFlag {
+		fmt.Println("--dev")
+		pageServer = PageRerender(pageServer)
+	}
+
+	http.Handle("/", pageServer)
 	http.Handle("/assets/", http.StripPrefix("/assets", NoCache(fs)))
 	http.HandleFunc("/connect", connect)
 
